960ee5853df7b833132264718b0fb8c995c25279
JSHayk <ayvazyan.555666@gmail.com>
2022-07-14 20:28:43 +0400

    my javascript app

---
+++ JS/product-content.js
@@ -0,0 +1,306 @@
+const productTypeItemsArr = [
+    {
+        image: './pictures/product-choose-images/ring.png',
+        info : 'Ring',
+        productItemsArr: [
+            {
+                image: './pictures/product-items/product-1.png',
+                model: 'A555',
+                price: '1700',
+                valuet: '$',
+                date: '06/14/2022',
+                id: 1
+            },
+            {
+                image: './pictures/product-items/product-2.png',
+                model: 'A456',
+                price: '200',
+                valuet: '$',
+                date: '06/13/2022',
+                id: 2
+            },
+            {
+                image: './pictures/product-items/product-3.png',
+                model: 'B894',
+                price: '1400',
+                valuet: '$',
+                date: '06/12/2022',
+                id: 3
+            },
+            {
+                image: './pictures/product-items/product-4.png',
+                model: 'C657',
+                price: '3000',
+                valuet: '$',
+                date: '06/11/2022',
+                id: 4
+            },
+            {
+                image: './pictures/product-items/product-5.png',
+                model: 'C789',
+                price: '2500',
+                valuet: '$',
+                date: '06/10/2022',
+                id: 5
+            },
+            {
+                image: './pictures/product-items/product-6.png',
+                model: 'D447',
+                price: '25060',
+                valuet: '$',
+                date: '06/09/2022',
+                id: 6
+            },
+            {
+                image: './pictures/product-items/product-7.png',
+                model: 'B112',
+                price: '9500',
+                valuet: '$',
+                date: '06/08/2022',
+                id: 7
+            },
+            {
+                image: './pictures/product-items/product-8.png',
+                model: 'A889',
+                price: '2500',
+                valuet: '$',
+                date: '06/07/2022',
+                id: 8
+            },
+            {
+                image: './pictures/product-items/product-9.png',
+                model: 'Z555',
+                price: '112',
+                valuet: '$',
+                date: '06/06/2022',
+                id: 9
+            },
+            {
+                image: './pictures/product-items/product-10.png',
+                model: 'V232',
+                price: '3245',
+                valuet: '$',
+                date: '06/05/2022',
+                id: 10
+            },
+            {
+                image: './pictures/product-items/product-11.png',
+                model: 'Z121',
+                price: '2254',
+                valuet: '$',
+                date: '06/04/2022',
+                id: 11
+            },
+            {
+                image: './pictures/product-items/product-12.png',
+                model: 'N002',
+                price: '652',
+                valuet: '$',
+                date: '06/03/2022',
+                id: 12
+            },
+            {
+                image: './pictures/product-items/product-13.png',
+                model: 'W474',
+                price: '2500',
+                valuet: '$',
+                date: '06/02/2022',
+                id: 13
+            },
+            {
+                image: './pictures/product-items/product-14.png',
+                model: 'E898',
+                price: '741',
+                valuet: '$',
+                date: '06/01/2022',
+                id: 14
+            },
+            {
+                image: './pictures/product-items/product-15.png',
+                model: 'R141',
+                price: '324',
+                valuet: '$',
+                date: '05/31/2022',
+                id: 15
+            },
+            {
+                image: './pictures/product-items/product-16.png',
+                model: 'A551',
+                price: '256',
+                valuet: '$',
+                date: '05/30/2022',
+                id: 16
+            },
+            {
+                image: './pictures/product-items/product-17.png',
+                model: 'Q414',
+                price: '652',
+                valuet: '$',
+                date: '05/29/2022',
+                id: 17
+            },
+            {
+                image: './pictures/product-items/product-18.png',
+                model: 'T221',
+                price: '142',
+                valuet: '$',
+                date: '05/28/2022',
+                id: 18
+            },
+            {
+                image: './pictures/product-items/product-19.png',
+                model: 'U797',
+                price: '978',
+                valuet: '$',
+                date: '05/27/2022',
+                id: 19
+            },
+            {
+                image: './pictures/product-items/product-20.png',
+                model: 'I777',
+                price: '875',
+                valuet: '$',
+                date: '05/26/2022',
+                id: 20
+            },
+            {
+                image: './pictures/product-items/product-21.png',
+                model: 'H474',
+                price: '6698',
+                valuet: '$',
+                date: '05/25/2022',
+                id: 21
+            },
+            {
+                image: './pictures/product-items/product-22.png',
+                model: 'O252',
+                price: '452',
+                valuet: '$',
+                date: '05/24/2022',
+                id: 22
+            },
+            {
+                image: './pictures/product-items/product-23.png',
+                model: 'D784',
+                price: '110',
+                valuet: '$',
+                date: '05/23/2022',
+                id: 23
+            },
+            {
+                image: './pictures/product-items/product-24.png',
+                model: 'U120',
+                price: '239',
+                valuet: '$',
+                date: '05/22/2022',
+                id: 24
+            },
+            {
+                image: './pictures/product-items/product-25.png',
+                model: 'N585',
+                price: '440',
+                valuet: '$',
+                date: '05/21/2022',
+                id: 25
+            },
+            {
+                image: './pictures/product-items/product-26.png',
+                model: 'V141',
+                price: '3456',
+                valuet: '$',
+                date: '05/20/2022',
+                id: 26
+            },
+            {
+                image: './pictures/product-items/product-27.png',
+                model: 'F444',
+                price: '875',
+                valuet: '$',
+                date: '05/19/2022',
+                id: 27
+            },
+            {
+                image: './pictures/product-items/product-28.png',
+                model: 'S555',
+                price: '952',
+                valuet: '$',
+                date: '05/18/2022',
+                id: 28
+            },
+            {
+                image: './pictures/product-items/product-29.png',
+                model: 'X122',
+                price: '322',
+                valuet: '$',
+                date: '05/17/2022',
+                id: 29
+            },
+            {
+                image: './pictures/product-items/product-30.png',
+                model: 'J998',
+                price: '80',
+                valuet: '$',
+                date: '05/16/2022',
+                id: 30
+            },
+        ],
+        subTypes: ['Ring', 'Wedding', 'Castet Ring', 'Coctail', 'Engagment'],
+        id: 1
+    },
+    {
+        image: './pictures/product-choose-images/kole.png',
+        info: 'Kole',
+        productItemsArr: [],
+        subTypes: [],
+        id: 2
+    },
+    {
+        image: './pictures/product-choose-images/earring.png',
+        info: 'Earring',
+        productItemsArr: [],
+        subTypes: [],
+        id: 3
+    },
+    {
+        image: './pictures/product-choose-images/culone.png',
+        info: 'Culone',
+        productItemsArr: [],
+        subTypes: [],
+        id: 4
+    },
+    {
+        image: './pictures/product-choose-images/chain.png',
+        info: 'Chain',
+        productItemsArr: [],
+        subTypes: [],
+        id: 5
+    },
+    {
+        image: './pictures/product-choose-images/brooch.png',
+        info: 'Brooch',
+        productItemsArr: [],
+        subTypes: [],
+        id: 6
+    },
+    {
+        image: './pictures/product-choose-images/bracelet.png',
+        info: 'Bracelet',
+        productItemsArr: [],
+        subTypes: [],
+        id: 7
+    }
+];
+
+const productTypeGenderArr = [
+    {
+        type: 'Woman',
+        image: './pictures/icons/product-woman.png'
+    },
+    {
+        type: 'Man',
+        image: './pictures/icons/product-man.png'
+    }
+];
+
+
+export {productTypeItemsArr, productTypeGenderArr}
